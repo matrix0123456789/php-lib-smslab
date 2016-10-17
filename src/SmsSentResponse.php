5ba782f82831e3cb462f442b5370e9afd8d442fe
Janusz Pyzio <janusz.pyzio@firma.interia.pl>
2016-10-17 13:27:51 +0200

    SMS Status Response Object

---
+++ src/SmsSentResponse.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Ittools\Smslabs;
+
+
+class SmsSentResponse
+{
+    private $account;
+    private $smsId;
+
+    /**
+     * SmsSentResponse constructor.
+     * @param int $account
+     * @param string $smsId
+     */
+    public function __construct($account, $smsId)
+    {
+        $this->account = $account;
+        $this->smsId = $smsId;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAccount()
+    {
+        return $this->account;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSmsId()
+    {
+        return $this->smsId;
+    }
+}
