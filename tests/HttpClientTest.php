27e98b008a14b40cd6c32a84e5da1ed55e062dbf
Janusz Pyzio <janusz@pyzio.eu>
2016-10-24 03:15:35 +0200

    Tests

--- tests/HttpClientTest.php
+++ tests/HttpClientTest.php
@@ -2,7 +2,6 @@
 
 namespace Ittools\Smslabs\Tests;
 
-
 use GuzzleHttp\Exception\ClientException;
 use Ittools\Smslabs\HttpClient;
 
@@ -10,7 +9,7 @@ class HttpClientTest extends \PHPUnit_Framework_TestCase
 {
     private $client;
 
-    function testHttpClientInvalidCredentials()
+    public function testHttpClientInvalidCredentials()
     {
         $this->expectException(ClientException::class);
         $this->client = new HttpClient('invalidKey', 'invalidSecret');
