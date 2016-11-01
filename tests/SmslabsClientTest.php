2d9dd3bb4929c068e4e3278d544342c863cc822f
Janusz Pyzio <janusz@pyzio.eu>
2016-11-01 02:21:11 +0100

    100% tests

--- tests/SmslabsClientTest.php
+++ tests/SmslabsClientTest.php
@@ -1,7 +1,11 @@
 <?php
 
-namespace Ittoolspl\Smslabs\Tests;
+namespace Ittoolspl\Smslabs\tests;
 
+use Ittoolspl\Smslabs\Entity\Sender;
+use Ittoolspl\Smslabs\Entity\SmsDetails;
+use Ittoolspl\Smslabs\Entity\SmsIn;
+use Ittoolspl\Smslabs\Entity\SmsOut;
 use Ittoolspl\Smslabs\Exception\EmptySMSQueueException;
 use Ittoolspl\Smslabs\HttpClient;
 use Ittoolspl\Smslabs\SmslabsClient;
@@ -22,6 +26,7 @@ class SmslabsClientTest extends \PHPUnit_Framework_TestCase
             'flash' => true,
             'expiration' => 10,
             'sender_id' => 'ITtools',
+            'send_date' => 1477960066
         ],
     ];
 
@@ -32,10 +37,13 @@ class SmslabsClientTest extends \PHPUnit_Framework_TestCase
 
     public function testSmslabsClientAddSmsToQueueValid()
     {
+        $sendDateTime = new \DateTime();
+        $sendDateTime->setTimestamp($this->validResult[1]['send_date']);
+
         $sms = new  SmslabsClient('', '');
         $sms->setSenderId('ITtools');
         $sms->add('+48790000000', 'Top secret SMS');
-        $sms->add('+48790000000', 'message', true, 10);
+        $sms->add('+48790000000', 'message', true, 10, $sendDateTime);
         $queue = $sms->getSmsQueue();
 
         $this->assertTrue($this->validResult == $queue);
@@ -100,6 +108,11 @@ class SmslabsClientTest extends \PHPUnit_Framework_TestCase
 
     public function testSmslabsClientSendValid()
     {
+        $response = [
+            'account' => 1593,
+            'sms_id' => '5813f6f4b5ca20c1767b23ca',
+        ];
+
         $sendDate = new \DateTime();
 
         $sms = new SmslabsClient('', '');
@@ -108,7 +121,7 @@ class SmslabsClientTest extends \PHPUnit_Framework_TestCase
         $sms->setExpirationMinutes(900);
         $sms->setSendDateTime($sendDate);
         $sms->add('+48790000000', 'message');
-        $sms->setClient($this->mockHttpClientSendSms());
+        $sms->setClient($this->mockHttpClient($response));
         $sms->send();
 
         $status = $sms->getSentStatus();
@@ -175,17 +188,145 @@ class SmslabsClientTest extends \PHPUnit_Framework_TestCase
         $sms->send();
     }
 
-    private function mockHttpClientSendSms()
+    public function testSmslabsClientGetAvailableSendersValid()
     {
-        $account = [
-            'account' => 1593,
-            'sms_id' => '5813f6f4b5ca20c1767b23ca',
+        $response = [
+            0 => ['id' => '57cdab26b5c123f15aaaaf1b', 'name' => 'ITTools.pl', 'sender' => '48111111111'],
+            1 => ['id' => '58168706b5c123216aaaa6e0', 'name' => 'ITtools', 'sender' => '48000000000'],
+            2 => ['id' => '57cec6feb5c123453aaaaf1d', 'name' => 'ITTools', 'sender' => 'ITTools'],
+            3 => ['id' => '581686c5b5c123666aaaa6de', 'name' => 'ITtools', 'sender' => 'ITtools'],
         ];
 
+        $sms = new SmslabsClient('', '');
+        $sms->setClient($this->mockHttpClient($response));
+        $senders = $sms->getAvailableSenders();
+
+        $this->assertCount(4, $senders);
+        $this->assertInstanceOf(Sender::class, $senders[0]);
+        $this->assertEquals('57cdab26b5c123f15aaaaf1b', $senders[0]->getId());
+        $this->assertEquals('ITtools', $senders[3]->getName());
+        $this->assertEquals('48000000000', $senders[1]->getSender());
+    }
+
+    public function testSmslabsClientGetAccountBalanceValid()
+    {
+        $response = ['account' => 1526];
+
+        $sms = new SmslabsClient('', '');
+        $sms->setClient($this->mockHttpClient($response));
+        $balance = $sms->getAccountBalance();
+
+        $this->assertEquals(15.26, $balance->getBalance());
+    }
+
+    public function testSmslabsClientGetSmsInValid()
+    {
+        $response = [
+            0 => [
+                '_id' => '5800fb5ab5ca20fb5cc03887',
+                's_cnt' => 1,
+                's_con' => 'Dzieki',
+                'no_to' => '48111111111',
+                'in_t' => 1476459354,
+                'no_fr' => '48000000000',
+                'stat' => 3,
+            ],
+            1 => [
+                '_id' => '57cec738b5ca20a359291f1b',
+                's_cnt' => 1,
+                's_con' => 'Test',
+                'no_to' => '48111111111',
+                'in_t' => 1473169208,
+                'no_fr' => '48000000000',
+                'stat' => 3,
+            ],
+        ];
+
+        $sms = new SmslabsClient('', '');
+        $sms->setClient($this->mockHttpClient($response));
+        $smsIn = $sms->getSmsIn();
+
+        $this->assertCount(2, $smsIn);
+        $this->assertInstanceOf(SmsIn::class, $smsIn[0]);
+        $this->assertEquals('57cec738b5ca20a359291f1b', $smsIn[1]->getId());
+        $this->assertEquals('Dzieki', $smsIn[0]->getContent());
+    }
+
+    public function testSmslabsClientGetSmsOutValid()
+    {
+        $response = [
+            0 => [
+                '_id' => '58050a182134242142341721',
+                'del_t' => 1476725279,
+                's_cnt' => 1,
+                'price' => 795,
+                's_con' => 'test',
+                'no_to' => '+48111111111',
+                'in_t' => 1476725272,
+                'stat' => 3,
+                'stat_d' => 'OK',
+                'no_fr' => 'ITTools',
+            ],
+            1 => [
+                '_id' => '523435345235354354581720',
+                'del_t' => 1476725218,
+                's_cnt' => 1,
+                'price' => 795,
+                's_con' => 'test',
+                'no_to' => '+48790222500',
+                'in_t' => 1476725207,
+                'stat' => 3,
+                'stat_d' => 'OK',
+                'no_fr' => 'ITtools',
+            ],
+        ];
+
+        $sms = new SmslabsClient('', '');
+        $sms->setClient($this->mockHttpClient($response));
+        $smsOut = $sms->getSmsOut();
+
+        $this->assertCount(2, $smsOut);
+        $this->assertInstanceOf(SmsOut::class, $smsOut[0]);
+        $this->assertEquals('523435345235354354581720', $smsOut[1]->getId());
+        $this->assertEquals('+48111111111', $smsOut[0]->getNumberTo());
+    }
+
+    public function testSmslabsClientGetSmsDetailsValid()
+    {
+        $response = [
+            'id' => '58047f3eb5ca207a48581725',
+            'from' => 'ITTools',
+            'number_to' => '+48790222500',
+            'status' => 3,
+            'sms_count' => 1,
+            'sms_content' => 'test',
+            'price' => 795,
+        ];
+
+        $sms = new SmslabsClient('', '');
+        $sms->setClient($this->mockHttpClient($response));
+        $smsDetails = $sms->getSmsDetails('58047f3eb5ca207a48581725');
+
+        $this->assertInstanceOf(SmsDetails::class, $smsDetails);
+        $this->assertEquals('58047f3eb5ca207a48581725', $smsDetails->getId());
+        $this->assertEquals('ITTools', $smsDetails->getFrom());
+    }
+
+    public function testSmslabsClientGetSmsDetailsInvalidID()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid SMS ID');
+
+        $sms = new SmslabsClient('', '');
+        $sms->getSmsDetails('test');
+    }
+
+    private function mockHttpClient(array $response)
+    {
         $httpClientMock = \Mockery::mock(HttpClient::class);
         $httpClientMock
             ->shouldReceive('sendRequest')
-            ->andReturn($account);
+            ->andReturn($response);
 
         return $httpClientMock;
     }
